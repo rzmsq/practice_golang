9b2f1d6a99592a0860bed84dc16d492a63ef30e6
rzmsq <semin.02@mail.ru>
2025-08-07 13:55:53 +0300

    logs

---
+++ customLogLineNumber/customLogLineNumber.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+)
+
+func main() {
+	LOGFILE := path.Join(os.TempDir(), "mGo.log")
+	f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	LstdFlags := log.Ldate | log.Lshortfile
+	iLog := log.New(f, "LNum ", LstdFlags)
+	iLog.Println("Hello world!")
+	iLog.SetFlags(log.Lshortfile | log.LstdFlags)
+	iLog.Println("Hello there!")
+}
